c0aa0f81d5ce249e4e20b84b287bf2b0a82cdf53
PrimakMV <pm2710@mail.ru>
2024-11-14 22:58:58 +0300

    [SF-B-J-006] Added logic for linking with the authorization microservice

---
+++ post/src/main/java/com/post/connector/AuthConnector.java
@@ -0,0 +1,12 @@
+package com.post.connector;
+
+import com.post.bom.UserBom;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
+
+@FeignClient(name = "auth-connector", url = "${connector.auth.url}")
+public interface AuthConnector {
+    @GetMapping(path = "/getCurrentUser")
+    UserBom getCurrentUser(@RequestHeader(name = "Authorization") String token);
+}
