955a9c8bbf0ebb865dfcc872f9cdbade794cfcbc
PrimakMV <pm2710@mail.ru>
2024-12-23 01:54:21 +0300

    [SF-B-J-010] Added the necessary models for likes

---
+++ feedback/src/main/java/com/feedback/bom/UserBom.java
@@ -0,0 +1,10 @@
+package com.feedback.bom;
+
+import lombok.Data;
+
+@Data
+public class UserBom {
+    private Long id;
+    private String username;
+    private String email;
+}
---
+++ feedback/src/main/java/com/feedback/model/Media.java
@@ -0,0 +1,27 @@
+package com.feedback.model;
+
+import jakarta.persistence.*;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@NoArgsConstructor
+@Table(name = "media")
+public class Media {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "post_id", nullable = false)
+    private Post post;
+
+    @Column(name = "media_type", nullable = false)
+    private String mediaType;
+
+    @Column(name = "url", nullable = false)
+    private String url;
+}
+
