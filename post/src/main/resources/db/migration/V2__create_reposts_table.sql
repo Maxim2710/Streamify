f5f088dd63ed6a2b3dfbe02e8d058061b868b951
PrimakMV <pm2710@mail.ru>
2024-12-22 23:59:46 +0300

    [SF-B-J-008] Added changes to the structure

--- post/src/main/resources/db/migration/V2__create_reposts_table.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE reposts (
-                         id SERIAL PRIMARY KEY,
-                         original_post_id BIGINT NOT NULL,
-                         user_id BIGINT NOT NULL,
-                         created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-                         FOREIGN KEY (original_post_id) REFERENCES posts(id),
-                         FOREIGN KEY (user_id) REFERENCES users(id)
-);
